31c11d3c42f659e20f00db141e1ef07e254dc671
Vanessa O <vaneolipa@gmail.com>
2022-09-22 11:27:05 -0500

    Add about.js file

---
+++ about.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector('.mobile-menu');
+const menuOverlay = document.querySelector('.overlay-wrap');
+const closeMobileMenu = document.querySelector('.cancel-wrap');
+const itemMobileMenu = document.querySelectorAll('.item-menu');
+
+menuBtn.addEventListener('click', () => {
+    menuOverlay.style.display = 'block';
+});
+
+closeMobileMenu.addEventListener('click', () => {
+    menuOverlay.style.display = 'none';
+});
+
+itemMobileMenu.forEach(element => {
+    element.addEventListener('click', () => {
+        menuOverlay.style.display = 'none';
+    });
+});
